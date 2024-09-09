72829b06a198633e3bddf17701e6aa1abd78b1ea
acolon114 <acolon114@usf.edu>
2024-09-09 02:29:29 +0000

    Develop findTopAndBottomPerformers function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -7,7 +7,7 @@ function calculateAverageSales(salesArray) {
 }
 
 const saleNumbers =[5, 35, 40];
-console.log("Average Sales Data: $" + calculateAverageSales(saleNumbers)); //Output: Average Sales Data: $26.666666666666668 
+console.log("Average Sales Data: $" + calculateAverageSales(saleNumbers).toFixed(2)); //Output: Average Sales Data: $26.67 
 
 // Task 2: Create a Function to Determine Performance Rating
 
@@ -26,3 +26,28 @@ function determinePerformanceRating(avgSales) {
     return performanceRatingOut;
     }
 }
+
+//Task 3: Create a Function to Identify Top and Bottom Performers
+
+function findTopAndBottomPerformers(salesPeople) {
+    const topPerf =
+    salesPeople.reduce((top, curr) => curr.totalSales > top.totalSales ?
+    curr: top, salesPeople[0]); // This function allows the program to identify the top performer
+
+    const bottomPerf =
+    salesPeople.reduce((bottom, curr) => curr.totalSales < bottom.totalSales ?
+    curr: bottom, salesPeople[0]); // This function allows the program to indenify the bottom performer
+
+    const performers =
+    {topPerformer: topPerf, 
+        bottomPerformer: bottomPerf
+    };
+    return performers;
+}
+
+let salesPeople = [
+    {name: "Alicia", totalSales: 6000},
+    {name: "Joe", totalSales: 10000},
+    {name: "Dave", totalSales: 15000},
+    {name: "Greg", totalSales: 13000}
+]; //This is an array of different objects representing sales people and their total sales
