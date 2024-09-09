11f41382ebc9b536c2d00104e473c9cb5c88fd8b
acolon114 <acolon114@usf.edu>
2024-09-09 02:12:59 +0000

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -8,3 +8,20 @@ function calculateAverageSales(salesArray) {
 
 const saleNumbers =[5, 35, 40];
 console.log("Average Sales Data: $" + calculateAverageSales(saleNumbers)); //Output: Average Sales Data: $26.666666666666668 
+
+// Task 2: Create a Function to Determine Performance Rating
+//const avgSales = calculateAverageSales(salesData);
+function determinePerformanceRating(avgSales) {
+    let performanceRatingOut;
+    if (avgSales > 10000) {
+        performanceRatingOut = "Excellent"; // Output: If average sales are > $10,000 then the rating is excellent
+    } else if (avgSales >= 7000) {
+        performanceRatingOut = "Good"; // Output: If average sales are < or = $7,000 then the rating is good
+    } else if (avgSales >= 4000) {
+        performanceRatingOut = "Satisfactory"; // Output: If average sales are > or = $4,000 then the rating is satisfactory
+    } else if (avgSales>= 0) {
+        performanceRatingOut = "Needs Improvement"; // Output: If average sales are > or = $0 then the rating needs improvement
+    } else { // Error handling for negative values
+        performanceRatingOut = "Invalid data"; // Output: If average sales are negative then the data is invalid
+    return performanceRatingOut;
+}
